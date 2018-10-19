6968974682691b03957fa4b7babecc3eb58d4b37
Jonathan Gastón Löwenstern <jonigl@gmail.com>
2018-10-19 17:59:11 -0300

    express added.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
+const packageInfo = require('./package.json');
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.json({ version: packageInfo.version });
+});
+
+const server = app.listen(process.env.PORT, function () {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log('Web server started at http://%s:%s', host, port);
+});
 
 bot.on('*', (msg) => msg.reply.text('Hi friend!'));
 
